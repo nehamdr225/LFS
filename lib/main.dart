04631295976bcc3af017f1ca83ed43b3b8ea9ce1
nehamdr225 <nehamdr225@gmail.com>
2019-08-24 20:12:07 +0545

    project setup

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main()=> runApp(
+  HomePage()
+);
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
