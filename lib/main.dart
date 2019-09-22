77e8b6eeb4b173b2db28043d105be1c5ad076b5d
codedump00 <blazinasis@gmail.com>
2019-09-22 14:12:33 +0545

    abstracted main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,19 @@ import 'package:provider/provider.dart';
 import 'package:LFS/pages/signinNsignup/UserPrompt.dart';
 import 'package:LFS/state/theme.dart';
 
-void main() => runApp(MainPage());
+void main() => runApp(BootStrapper());
 
-class MainPage extends StatelessWidget {
+class BootStrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(builder: (context) => FTheme())],
+      child: LfsApp(),
+    );
+  }
+}
+
+class LfsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Provider.of<FTheme>(context).getTheme();
