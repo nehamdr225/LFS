6864579e952f272c31b9dd7c75745eb755245a08
codedump00 <blazinasis@gmail.com>
2019-10-22 15:22:48 +0545

    layed out rough layout for fav page

--- lib/pages/FavouritesPage.dart
+++ lib/pages/FavouritesPage.dart
@@ -1,5 +1,9 @@
 import 'package:LFS/widget/atoms/Appbar.dart';
+import 'package:LFS/state/user.dart';
+import 'package:LFS/state/merchants.dart';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class FavouritesPage extends StatefulWidget {
   FavouritesPage({Key key}) : super(key: key);
@@ -10,6 +14,9 @@ class FavouritesPage extends StatefulWidget {
 class _FavouritesPageState extends State<FavouritesPage> {
   @override
   Widget build(BuildContext context) {
+    final MerchantsModel merchants = Provider.of<MerchantsModel>(context);
+    final List favourites = Provider.of<UserModel>(context).favourites;
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(50.0),
@@ -19,7 +26,27 @@ class _FavouritesPageState extends State<FavouritesPage> {
           title: "Favourites",
         ),
       ),
-      body: ListView(),
+      body: ListView(
+        children: <Widget>[
+          favourites.length > 0
+              ? favourites.map((fav) {
+                  final merchant = merchants.one(fav);
+                  return Row(
+                    children: <Widget>[
+                      Text(merchant['name']),
+                      Text(merchant['address'])
+                    ],
+                  );
+                })
+              : Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Your favourites list is empty.",
+                    style: TextStyle(fontFamily: "Helvetica"),
+                  ),
+                ),
+        ],
+      ),
     );
   }
 }
